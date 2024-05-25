ea6525991bf26618542722be96d8d7e0d6b14225
vr-varad <varadgupta21#gmail.com>
2024-05-25 23:21:54 +0530

    Initial Commit: Added Initial Files

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+require('dotenv').config
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT,()=>{
+    console.log("Server started at port",PORT)
+})
