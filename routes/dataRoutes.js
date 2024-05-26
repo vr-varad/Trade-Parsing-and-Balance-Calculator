ff634ca3bc96cf5eb0ba153d5ea158de18014272
vr-varad <varadgupta21#gmail.com>
2024-05-26 12:44:25 +0530

    Added root endpoint message with available routes

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -8,6 +8,19 @@ const router = express.Router();
 
 
 const dataRoutes = (upload) => {
+    router.get('/',(req,res)=>{
+        return res.send(
+            `<h1>Welcome to the Data API</h1>
+            <p>This is the root endpoint of the Data API.</p>
+            <p>Available routes:</p>
+            <ul>
+                <li><a href="/api/v1/upload" method="POST">Upload Data</a></li>
+                <p>an API that accepts a CSV file as the input, parses the data present in it and stores it in a database</p>
+                <li><a href="/api/v1/balance">Get Asset Balance</a></li>
+                <p>an API that would give the asset-wise balance of the account at any given timestamp.</p>
+            </ul>`
+        );
+    })
     router.post('/upload', upload.single('file'), uploadAndStoreCsvData);
     router.get('/balance', getAssetBalanceAtTimestamp);
     return router;
